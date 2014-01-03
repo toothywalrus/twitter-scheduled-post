ef2312118ee5507975d5ec430567cb0df295eac3
toothywalrus <suhanovpavel@gmail.com>
2014-01-03 18:01:06 +0200

    init

--- celery_test/api/api.py
+++ celery_test/api/api.py
@@ -2,24 +2,6 @@ from rest_framework import viewsets, generics
 
 from .models import WebPage, Link
 from .serializers import WebPageSerializer, LinkSerializer
-
-# class WebPageDetail(generics.RetrieveUpdateDestroyAPIView):
-#     model = WebPage
-#     serializer_class = WebPageSerializer
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
-
-# class WebPageList(generics.ListAPIView):
-#     model = WebPage
-#     serializer_class = WebPageSerializer
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
-
-class WebPageViewSet(viewsets.ModelViewSet):
-	queryset = WebPage.objects.all()
-	serializer_class = WebPageSerializer
 	
 class PageLinkList(generics.ListAPIView):
     model = Link
