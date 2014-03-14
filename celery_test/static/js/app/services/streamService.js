71d3bb66aff42dc2161be381e7bb7cf4a5025ee3
toothywalrus <suhanovpavel@gmail.com>
2014-03-14 17:09:06 +0200

    improvements to createForm and modelRelations insides (added some functions)

--- celery_test/static/js/app/services/streamService.js
+++ celery_test/static/js/app/services/streamService.js
@@ -5,7 +5,6 @@ angular.module('Posting').factory('streamService', function(Info) {
 
     source.addEventListener('saved', function(e) {
         var data = JSON.parse(e.data);
-        console.log(e.data);
         Info.addToInfo(data.model_name, data.item);
     });
 
