789e67a4cfcd20a7d54864572d1b8e5048fdb775
toothywalrus <suhanovpavel@gmail.com>
2014-03-17 13:33:22 +0200

    deleted old files

--- celery_test/static/js/app/services/api.js
+++
@@ -1,9 +0,0 @@
-var api = angular.module('celery_test.api', ['ngResource']);
-
-api.factory('Tweet', ['$resource', function ($resource) {
-    return $resource('/tweets/tweets/:id', {id: '@id'});
-}]);
-
-api.factory('TimedTweet', ['$resource', function ($resource) {
-    return $resource('/tweets/timedtweets/:id', {id: '@id'});
-}]);
